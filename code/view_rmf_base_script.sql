fdfac8e00a1d3c60a8b143d01336d765f1a5c208
Lily-ThuPham <thuphamanh.da@gmail.com>
2025-11-21 10:23:34 +0700

    Enhance Customer Segmentation Notebook and SQL View
    - Updated the Customer Segmentation notebook to include a description of the K-Means model for customer segmentation.
    - Adjusted execution counts for code cells to maintain proper order.
    - Removed unnecessary output from the data sanity check cell.
    - Added a new section for saving the K-Means model and scaler to the models directory.
    - Introduced a scoring system for Recency-Monetary-Frequency (R-M-F) to facilitate business logic and visualization.
    - Created a new SQL script for the dynamic Recency-Monetary-Frequency scoring view, ensuring it calculates recency based on the latest date in the dataset.

---
+++ code/view_rmf_base_script.sql
@@ -0,0 +1,37 @@
+-- Customer Segmentation - Dynamic Recency-Monetary-Frequency Scoring view 
+USE db_olist_analytics;
+
+-- Remove older view if existed 
+DROP VIEW IF EXISTS view_rmf_base; 
+
+-- Recency is calculated using the current date but since this is a project with statis dataset, I'll use the latest date in the dataset.
+-- Find the latest date in the dataset to calculate recency.
+WITH latest_date as (
+SELECT 
+	GREATEST(
+		MAX(order_purchase_timestamp),
+        MAX(order_approved_at),
+        MAX(order_delivered_carrier_date),
+        MAX(order_delivered_customer_date),
+        MAX(order_estimated_delivery_date)
+        ) as latest_date
+FROM fct_orders)
+
+-- Create the view 
+CREATE VIEW view_rmf_base as 
+SELECT 
+	c.customer_unique_sk,
+    -- RECENCY
+	DATEDIFF(
+		(SELECT latest_date FROM latest_date),MAX(o.order_purchase_timestamp)) as recency,
+	-- Frequecy
+    COUNT(o.order_sk) as frequency,
+    -- Monetary 
+    SUM(i.price) as monetary
+FROM dim_customers c 
+JOIN fct_orders o ON o.customer_sk = c.customer_sk 
+JOIN fct_order_items i ON i.order_sk = o.order_sk 
+WHERE o.order_status = 'delivered' 
+GROUP BY 1 
+;
+
